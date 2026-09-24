9007b2e111e7486aacf3f802ec581466e77a248b
Carlos Eduardo <eduardocarlosfariasluz3>
2026-09-24 19:39:31 -0300

    Core pega toda a tabela de info dos users, pega os nomes, junta numa string e envia para um email de teste

--- backend/core.py
+++ backend/core.py
@@ -4,25 +4,60 @@ import threading
 from dotenv import load_dotenv
 from Database import database
 from msg_core import CaixaDeMensagens, Mensagem
+import time
+import logging
 
 load_dotenv()
 email = os.getenv('EMAIL_USER')
 password = os.getenv('EMAIL_PASS')
+enviar = os.getenv('ENVIAR')
 yag = yagmail.SMTP(email, password)
 
 automations = database.all("automations","nome",all=True)
+log = logging.getLogger("CORE")
+log.setLevel(logging.INFO)
+log.propagate = False
 
+formatter = logging.Formatter('%(name)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
 
 class Core(threading.Thread):
     def __init__(self,msg:CaixaDeMensagens):
         super().__init__()
         self.msg = msg
         self.name = "core"
+        self.valores = []
+
+        for i in automations:
+            self.valores.append(i.nome)
+
+        log.info("Core iniciado")
 
     def run(self):
-            self.msg.enviar(Mensagem(
-                de=self.name,
-                para="api",
-                tipo="teste",
-                dados = {}
-            ))
+        while True:
+            receber = self.msg.receber(self.name)
+
+            if receber:
+                log.info("Recebi mensagens")
+                for dados in receber:
+                    self.valores.append(dados.dados["name"])
+
+            time.sleep(10)
+            enviarmsg = ""
+            for i in self.valores:
+                enviarmsg += i
+                enviarmsg += " "
+
+
+            log.info("enviando email")
+            global enviar
+            yag.send(
+                to=enviar,
+                subject="Mensagem do core",
+                contents=enviarmsg
+            )
+
+
+
